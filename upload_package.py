fa553330266d328977c526093b86b5abf4db79c5
hjyangchoi <hjyang@mediquitous.com>
2024-06-22 14:53:51 +0900

    Edit upload_package for specific dist info

--- upload_package.py
+++ upload_package.py
@@ -6,4 +6,5 @@ load_dotenv()
 TWINE_USERNAME = os.getenv('TWINE_USERNAME')
 TWINE_PASSWORD = os.getenv('TWINE_PASSWORD')
 
-os.system(f'twine upload dist/* -u {TWINE_USERNAME} -p {TWINE_PASSWORD}')
+os.system('python3 setup.py sdist bdist_wheel')
+os.system(f'twine upload --verbose dist/* -u {TWINE_USERNAME} -p {TWINE_PASSWORD}')
