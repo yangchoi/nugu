24b6fe6af16827912ee07e9536941cb60bf1602a
hjyangchoi <hjyang@mediquitous.com>
2024-06-22 14:54:32 +0900

    Update package version - More information README

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="nugu",
-    version="0.2",
+    version="0.3",
     packages=find_packages(),
     install_requires=[],
     author="YangChoi",
